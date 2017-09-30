70bf4cd2ba47c60145d912740a80a4b38a79cb0a
bullet64 <frankmankel@t-online.de>
2017-09-30 08:32:11 +0200

    Create eh.js

---
+++ eh.js
@@ -0,0 +1,18 @@
+function meinHandler(id, ereignis, funktion)
+
+{
+
+   if(window.addEventListener)
+
+      document.getElementById(id)
+
+         .addEventListener(ereignis, funktion, false);
+
+   else if(window.attachEvent)
+
+      document.getElementById(id)
+
+         .attachEvent("on" + ereignis, funktion);
+
+}
+
