9eb0e8c80c72fa1a278ce29d8662a0c8427a6296
bullet64 <frankmankel@t-online.de>
2017-09-30 15:57:26 +0200

    Update nennung.php

--- nennung.php
+++ nennung.php
@@ -19,8 +19,8 @@
       echo "Klasse: "             . $_POST["klasse"] . "<br>";
       echo "E-Mail: "             . $_POST["email"] . "<br>";
    
-	$statement = $pdo->prepare("INSERT INTO id (nachname, vorname, geburtstag) VALUES (?, ?, ?)");
-      	$statement->execute(array($_POST["nachname"], $_POST["vorname"], $_POST["geburtstag"]));
+	$statement = $pdo->prepare("INSERT INTO id (nachname, vorname, geburtstag, dmc, verein, transponder_id; klasse, email; car_id) VALUES (?, ?, ?, ?, ? ,? ,? ,? ,?);
+      	$statement->execute(array($_POST["nachname"], $_POST["vorname"], $_POST["geburtstag"], $_POST["dmc"], $_POST["verein"], $_POST["transponder_id"], $_POST["klasse"], $_POST["email"], $_POST["email"], $_POST["car_id"]));
 	
 	$pdo = null;
 
