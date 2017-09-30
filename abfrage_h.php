29c188ba10015a56165a1f37ca7e9b8754778aca
bullet64 <frankmankel@t-online.de>
2017-09-30 20:34:49 +0200

    Update abfrage_h.php

--- abfrage_h.php
+++ abfrage_h.php
@@ -19,7 +19,7 @@
 
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-$sql = "SELECT * FROM id";
+$sql = "SELECT * FROM idWHERE klasse = 'OR8 Hobby' ";
 ?>
 	
 <table>
