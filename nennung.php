fe514980140c7b593f9f8894e0b661681d02cfe6
bullet64 <frankmankel@t-online.de>
2017-09-30 12:28:18 +0200

    Update nennung.php

--- nennung.php
+++ nennung.php
@@ -19,18 +19,16 @@
       echo "Klasse: "             . $_POST["klasse"] . "<br>";
       echo "E-Mail: "             . $_POST["email"] . "<br>";
    
-      //Geburtstags zerlegen! Result = 28.08.1964 Input = 2017-09-27
-		//$_POST["geburtstag"] Geburtsdatum (2017-09-27
+	//Geburtstags zerlegen! Result = 28.08.1964 Input = 2017-09-27
+	//$_POST["geburtstag"] Geburtsdatum (2017-09-27
 			$datum = $_POST["geburtstag"]; // Geburtsdatum
 			$array = explode("-",$datum); //Datum zerlegen (2013-08-22)
 			$erg = $array[2].'.'.$array[1].'.'.$array[0];
-			//Irgendwo kamen zwei führende Punkte her. (..28.08.1964) Mit trim werden die aus dem String entfernt!
-			//$erg2 = trim($erg, ".");
-			//$results_arr[] =  $erg2; // Geburtsdatum in deutsch ausgeben!
+			
       echo "TEST: $erg";
 
       $statement = $pdo->prepare("INSERT INTO id (nachname, vorname, geburtstag) VALUES (?, ?, ?)");
-      $statement->execute(array($_POST["nachname"], $_POST["vorname"], $_POST["geburtstag"]));
+      $statement->execute(array($_POST["nachname"], $_POST["vorname"], $erg["geburtstag"]));
 
       $pdo = null;
 
