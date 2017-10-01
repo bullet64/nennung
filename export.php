8063a6983245506371fd813a3c4f245e1cfe31de
bullet64 <frankmankel@t-online.de>
2017-10-01 15:13:53 +0200

    Update export.php

--- export.php
+++ export.php
@@ -5,7 +5,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 $sql = "SELECT * FROM id";
 $sql1 = mysql_query($sql);
 
-while ($row=mysql_fetch_assoc(sql1)){
+while ($row=mysql_fetch_assoc($sql1)){
   echo "$row[id],$row[vorname],$row[nachname]<br>\n;
 
 
