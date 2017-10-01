a1819bc586f1241081017409c86f2fba953aa48d
bullet64 <frankmankel@t-online.de>
2017-10-01 14:41:24 +0200

    Update export.php

--- export.php
+++ export.php
@@ -4,20 +4,23 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 $sql = "SELECT * FROM id WHERE";
 
 //Header schreiben
-$header = array (
-	array('Transponder Nr 1', 'Transponder Nr 2', 'Liz.-Nr.', 'Name', 'Vorname', 'Club', 'Geburtstag', 'Aktive Frequenz', 'Synthesizer', 'Klasse', 'EMail', 'CarId 1', 'CarId 2', 'Land', 'Payment'),
-	);
+//$header = array (
+//	array('Transponder Nr 1', 'Transponder Nr 2', 'Liz.-Nr.', 'Name', 'Vorname', 'Club', 'Geburtstag', 'Aktive Frequenz', 'Synthesizer', 'Klasse', 'EMail', 'CarId 1', 'CarId 2', 'Land', 'Payment'),
+//	);
 
+$out = fopen('php://output', 'w');
+fputcsv($out, array('this','is some', 'csv "stuff", you know.'));
+fclose($out);
 
 //Schreiben der Daten
-$fp = fopen('file.csv', 'w');
+//$fp = fopen('file.csv', 'w');
 //print $csv_output; //UTF-8
 //print mb_convert_encoding( $csv_output, 'Windows-1252', 'AUTO');
-	foreach ($header as $fields) {
-		fputcsv($fp, $fields);
-	}
+//	foreach ($header as $fields) {
+//		fputcsv($fp, $fields);
+//	}
 
-fclose($fp);
+//fclose($fp);
 
 //DB close
 $pdo = null;
