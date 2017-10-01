493e56cacb68b0b8bc6b6c60e0fe226cc0f8d659
bullet64 <frankmankel@t-online.de>
2017-10-01 17:56:59 +0200

    Update export.php

--- export.php
+++ export.php
@@ -1,11 +1,46 @@
 <?php
 
+//Definitionen
 $abfrage = array();
+$filename = 'test.txt';
+$header = "Transponder Nr 1;Transponder Nr 2;Liz.-Nr;Name;Vorname;Club;Geburtstag;Aktive Frequenz;Synthesizer;Klasse;EMail;CarId 1;CarId 2;Land;Payment";
+
+
+//Header in Datei schreiben
+
+// Sichergehen, dass die Datei existiert und beschreibbar ist.
+if (is_writable($filename)) {
+
+    // Wir öffnen $filename im "Anhänge" - Modus.
+    // Der Dateizeiger befindet sich am Ende der Datei, und
+    // dort wird $somecontent später mit fwrite() geschrieben.
+    if (!$handle = fopen($filename, "a")) {
+         print "Kann die Datei $filename nicht öffnen";
+         exit;
+    }
+
+    // Schreibe $somecontent in die geöffnete Datei.
+    if (!fputcsv($handle, $header)) {
+        print "Kann in die Datei $filename nicht schreiben";
+        exit;
+    }
+
+    print "Fertig, in Datei $filename wurde $header geschrieben";
+
+    fclose($handle);
+
+} else {
+    print "Die Datei $filename ist nicht schreibbar";
+}
+
 
 //DB-Aufruf
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 
+//DB-Abfrage
 $sql = "SELECT * FROM id";
+
+//Schleife zur Ausgabe der Daten
 foreach ($pdo->query($sql) as $row) {
    echo $row['vorname']." ".$row['nachname']."<br />";
    $abfrage[] = $row['vorname']." ".$row['nachname'];
