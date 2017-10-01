7bc22d1649e7a369c53dfaa91dd00d143347a5ec
bullet64 <frankmankel@t-online.de>
2017-10-01 16:03:34 +0200

    Update export.php

--- export.php
+++ export.php
@@ -5,11 +5,11 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 
 $sql = "SELECT * FROM id";
 foreach ($pdo->query($sql) as $row) {
-   echo $row['vorname'].",".$row['nachname']."<br />";
-   $abfrage[] = $row['vorname'].",".$row['nachname']."<br />";
+   echo $row['vorname']." ".$row['nachname']."<br />";
+   $abfrage[] = $row['vorname']." ".$row['nachname']."<br />";
 }
 
-echo $abfrage[2][2];
+echo $abfrage[0][0];
 
 
 //DB close
