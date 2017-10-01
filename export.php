4adc06a8c26c1ee1fb663307d5472d7e7d6cd85a
bullet64 <frankmankel@t-online.de>
2017-10-01 19:30:09 +0200

    Update export.php

--- export.php
+++ export.php
@@ -1,4 +1,6 @@
 <?php
+// Daten Export im CSV-Format für RC Timing (http://www.rc-timing.ch/). Trennzeichen ";"
+
 
 //Definitionen
 $abfrage = array();
@@ -7,25 +9,24 @@ $header = array("Name","Vorname");
 
 
 //Header in Datei schreiben
-
 // Sichergehen, dass die Datei existiert und beschreibbar ist.
 if (is_writable($filename)) {
 
-    // Wir öffnen $filename im "Anhänge" - Modus.
-    // Der Dateizeiger befindet sich am Ende der Datei, und
-    // dort wird $somecontent später mit fwrite() geschrieben.
+    // Wir öffnen $filename im "Schreiben" - Modus. Datei wird komplett überschrieben!
+    // Der Dateizeiger befindet sich am Anfang der Datei, und
+    // dort wird $header später mit fputcsv() geschrieben.
     if (!$handle = fopen($filename, "w")) {
          print "Kann die Datei $filename nicht öffnen";
          exit;
     }
 
-    // Schreibe $somecontent in die geöffnete Datei.
+    // Schreibe $header in die geöffnete Datei. Trenne die Daten mit einem ";"
     if (!fputcsv($handle, $header, ';')) {
         print "Kann in die Datei $filename nicht schreiben";
         exit;
     }
 
-    print "Fertig, in Datei $filename wurde $header geschrieben";
+    print "Fertig, in Datei $filename wurde $header geschrieben"; // Kann später weg!
 
     fclose($handle);
 
@@ -42,11 +43,12 @@ $sql = "SELECT * FROM id";
 
 //Schleife zur Ausgabe der Daten
 foreach ($pdo->query($sql) as $row) {
-   echo $row['vorname'].";".$row['nachname']."<br />";
-   $abfrage[] = $row['vorname'].";".$row['nachname'];
-    
+   echo $row['vorname'].";".$row['nachname']."<br />"; // Kann später weg!
    
+// Abfrage bauen für den Export. Evt. müssen ein paar Daten angepasst werden.    
+    $abfrage[] = $row['vorname'].";".$row['nachname']; 
 
+// Daten schreiben
 // Sichergehen, dass die Datei existiert und beschreibbar ist.
 if (is_writable($filename)) {
 
@@ -71,7 +73,8 @@ if (is_writable($filename)) {
 } else {
     print "Die Datei $filename ist nicht schreibbar";
 }
-   $abfrage = array();
+// Das Array wieder leeren!
+    $abfrage = array();
 }
 
 
