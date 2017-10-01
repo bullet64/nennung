aa756cba466319f3fb9848bfc68d3bce84799cc1
bullet64 <frankmankel@t-online.de>
2017-10-01 16:35:45 +0200

    Update export.php

--- export.php
+++ export.php
@@ -23,7 +23,7 @@ if (is_writable($filename)) {
     }
 
     // Schreibe $somecontent in die geöffnete Datei.
-    if (!fwrite($handle, $abfrage)) {
+    if (!fputcsv($handle, $abfrage)) {
         print "Kann in die Datei $filename nicht schreiben";
         exit;
     }
