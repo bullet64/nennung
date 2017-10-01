a4b531844056e0139e120f15f67e13ea502dafb4
bullet64 <frankmankel@t-online.de>
2017-10-01 16:10:45 +0200

    Update export.php

--- export.php
+++ export.php
@@ -7,9 +7,37 @@ $sql = "SELECT * FROM id";
 foreach ($pdo->query($sql) as $row) {
    echo $row['vorname']." ".$row['nachname']."<br />";
    $abfrage[] = $row['vorname']." ".$row['nachname']."<br />";
+   
+   //in Datei
+   $filename = 'test.txt';
+   
+   // Sichergehen, dass die Datei existiert und beschreibbar ist.
+if (is_writable($filename)) {
+
+    // Wir öffnen $filename im "Anhänge" - Modus.
+    // Der Dateizeiger befindet sich am Ende der Datei, und
+    // dort wird $somecontent später mit fwrite() geschrieben.
+    if (!$handle = fopen($filename, "a")) {
+         print "Kann die Datei $filename nicht öffnen";
+         exit;
+    }
+   
+    // Schreibe $somecontent in die geöffnete Datei.
+    if (!fwrite($handle, $abfrage)) {
+        print "Kann in die Datei $filename nicht schreiben";
+        exit;
+    }
+   
+   print "Fertig, in Datei $filename wurde $abfrage geschrieben";      
+}
+   
+   fclose($handle);
+
+} else {
+    print "Die Datei $filename ist nicht schreibbar";
 }
 
-echo $sql[0][1];
+echo $abfrage[0][1];
 
 
 //DB close
