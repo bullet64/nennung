02ef3aec74b031be82fa50ecfbfc0a6685403f8d
bullet64 <frankmankel@t-online.de>
2017-10-01 15:04:07 +0200

    Update export.php

--- export.php
+++ export.php
@@ -4,6 +4,12 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 $sql = "SELECT * FROM id INTO OUTFILE 'mein.csv'";
 
 echo $sql[1];
+echo $sql[2];
+echo $sql[3];
+echo $sql[4];
+echo $sql[5];
+echo $sql[6];
+
 
 //DB close
 $pdo = null;
