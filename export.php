b45fc62d642ec3a05beceab5f987258ed75dd75c
bullet64 <frankmankel@t-online.de>
2017-10-01 15:45:16 +0200

    Update export.php

--- export.php
+++ export.php
@@ -3,7 +3,7 @@
 //DB-Aufruf
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 
-$sql = "SELECT * FROM id WHERE id = 1";
+$sql = "SELECT * FROM id";
 foreach ($pdo->query($sql) as $row) {
    echo $row['vorname']." ".$row['nachname']."<br />";
    echo "E-Mail: ".$row['email']."<br /><br />";
