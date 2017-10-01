f63b500f3213617be05fd10b019ff9eb9aa472d5
bullet64 <frankmankel@t-online.de>
2017-10-01 14:58:55 +0200

    Update export.php

--- export.php
+++ export.php
@@ -1,16 +1,24 @@
 <?php
 //DB-Aufruf
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-$sql = "SELECT * FROM id WHERE";
+$sql = "SELECT * FROM id INTO OUTFILE 'mein.csv';
+
+//foreach ($sql as $r)
+//{
+//			$results_arr = array();
+//			$results_arr[] = $fieldValues[$r->id][14]; // Transponder Nr.1
+ //     $csv_output .= "\r\n" . implode(";", $results_arr);
+	//	}	
+
+
+
 
 //Header schreiben
 //$header = array (
 //	array('Transponder Nr 1', 'Transponder Nr 2', 'Liz.-Nr.', 'Name', 'Vorname', 'Club', 'Geburtstag', 'Aktive Frequenz', 'Synthesizer', 'Klasse', 'EMail', 'CarId 1', 'CarId 2', 'Land', 'Payment'),
 //	);
 
-$out = fopen('php://output', 'w'); // $fp = fopen('file.csv', 'w');
-fputcsv($out, $sql));
-fclose($out);
+
 
 //Schreiben der Daten
 //$fp = fopen('file.csv', 'w');
