f30669b44516f2719d9a8e4276e91fc7bcc4e4cb
bullet64 <frankmankel@t-online.de>
2017-10-01 14:36:31 +0200

    Update export.php

--- export.php
+++ export.php
@@ -4,15 +4,19 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 $sql = "SELECT * FROM id WHERE";
 
 //Header schreiben
-//$csv_output = JText::_('Transponder Nr 1;Transponder Nr 2;Liz.-Nr;Name;Vorname;Club;Geburtstag;Aktive Frequenz;Synthesizer;Klasse;EMail;CarId 1;CarId 2;Land;Payment');
+$header = array('Transponder Nr 1', 'Transponder Nr 2', 'Liz.-Nr.', 'Name', 'Vorname', 'Club', 'Geburtstag', 'Aktive Frequenz', 'Synthesizer', 'Klasse', 'EMail', 'CarId 1', 'CarId 2', 'Land', 'Payment');
 
 
 
 //Schreiben der Daten
+$fp = fopen('file.csv', 'w');
 //print $csv_output; //UTF-8
 //print mb_convert_encoding( $csv_output, 'Windows-1252', 'AUTO');
-	
+	foreach ($header as $fields) {
+		fputcsv($fp, $fields);
+	}
 
+fclose($fp);
 
 //DB close
 $pdo = null;
