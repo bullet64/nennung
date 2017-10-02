52b723aa7997d5fc35b2e77b74be9e57d4737c0f
bullet64 <frankmankel@t-online.de>
2017-10-02 21:18:49 +0200

    Update test.php

--- test.php
+++ test.php
@@ -30,8 +30,7 @@
    
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-$stmt = $pdo->prepare('SELECT veranstaltung FROM vereine');
-$stmt->execute();
+$stmt = "SELECT veranstaltung FROM vereine";
 ?>
    
    <form method="post" action="nennung.php">
