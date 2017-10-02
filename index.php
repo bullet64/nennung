a7434bb2c96ea23c44842de6e1bfc2ea42c42f4a
bullet64 <frankmankel@t-online.de>
2017-10-03 00:21:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $stmt->execute();
     <td><input type="text" name="veranstaltung" list="veranstaltungen" required>
                <datalist id="veranstaltungen">
                   <?php
-                  while($result = $stmt->fetch(PDO::FETCH_Columns, 0)) { ?>
+                  while($result = $stmt->fetch(PDO::FETCH_COLUMNS, 0)) { ?>
                  <option value="<?php echo $result ?>" />
                   <?php
                     } 
