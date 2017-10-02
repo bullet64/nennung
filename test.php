4200cd5e68cbcd27f7c3d1f016e427fb1e2ac84a
bullet64 <frankmankel@t-online.de>
2017-10-02 18:30:07 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
+
+$stmt = $pdo->query('SELECT veranstaltung FROM vereine');
+foreach ($stmt as $row)
+{
+    echo $row['veranstaltung'] . "\n";
+}
+
+
+?>
