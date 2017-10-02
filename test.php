e9edc9964959f184e2afcc4616032016c68c345c
bullet64 <frankmankel@t-online.de>
2017-10-02 21:22:40 +0200

    Update test.php

--- test.php
+++ test.php
@@ -43,7 +43,7 @@ $stmt->execute();
     <td><input type="text" name="veranstaltung" list="veranstaltungen" required>
                <datalist id="veranstaltungen">
                   <?php
-                  while($result = $stmt->fetch()) { ?>
+                  while($result = $stmt->fetch(PDO::FETCH_COLUMN, 0)) { ?>
                  <option value="<?php echo $result ?>" />
                   <?php
                     } 
