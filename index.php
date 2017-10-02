f78ebbdaf51a63e59f8b01e13cf87738b433d66e
bullet64 <frankmankel@t-online.de>
2017-10-02 22:52:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,11 @@
       <li><a href="http://192.168.3.243/nennung/abfrage.php">Teilnehmerliste</a></li>
       <li><a href="http://192.168.3.243/nennung/abfrage_h.php">Klasse Hobby</a></li>
       <li><a href="http://192.168.3.243/nennung/abfrage_t.php">Klasse Top</a></li>
-      <li><a href="http://192.168.3.243/nennung/export.php">RCM Export</a></li>
+       <li><a href="http://192.168.3.243/nennung/admin.php">Admin</a></li>
+            <ul>
+               <li><a href="http://192.168.3.243/nennung/export_t.php">RCM Export</a></li>
+            </ul>   
+              
   </ul>
 </nav>
 
