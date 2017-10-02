9e88aa37ea8e9d1f6b8dd49ee8f043948731e75c
bullet64 <frankmankel@t-online.de>
2017-10-02 18:31:07 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
 $stmt = $pdo->query('SELECT veranstaltung FROM vereine');
 foreach ($stmt as $row)
 {
-    echo $row['veranstaltung'] . "\n";
+    echo $row['veranstaltung'] . "</p>";
 }
 
 
