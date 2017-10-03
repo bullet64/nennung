38de9ca31b2bd2a726246117bbb6c21d778987d0
bullet64 <frankmankel@t-online.de>
2017-10-03 12:52:59 +0200

    Update del_veranstaltung.php

--- del_veranstaltung.php
+++ del_veranstaltung.php
@@ -1,10 +1,25 @@
-   <?php
+  <!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Antwort vom Webserver</title>
+</head>
+<body><p> 
+
+<?php
       	$pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');           	
       	$sql = "DELETE FROM veranstaltungen WHERE veranstaltung =  :filmID";
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindParam(':filmID', $_POST['veranstaltung'], PDO::PARAM_INT);   
 	$stmt->execute();
 	$pdo = null;
+	echo "Die ausgewählte Veranstaltung $_POST['vernastaltung'] wurde gelöscht";
 	?>
 
 
+
+   
+
+</p>
+</body>
+</html>
