22988a1188be24735953767ecb8a5d1f19d30726
bullet64 <frankmankel@t-online.de>
2017-10-03 07:50:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -33,8 +33,10 @@
    
 <?php
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-$stmt = $pdo->prepare('SELECT verein FROM vereine SORT BY verein ASC');
-$stmt->execute();
+//$stmt = $pdo->prepare('SELECT verein FROM vereine');
+//$stmt->execute();
+$sql = "SELECT verein FROM vereine";
+
 ?>
    
     <form id="idForm" method="post" action="nennung.php">
@@ -46,8 +48,8 @@ $stmt->execute();
     <td><input type="text" name="veranstaltung" list="veranstaltungen" required>
                <datalist id="veranstaltungen">
                   <?php
-                  while($result = $stmt->fetch(PDO::FETCH_COLUMN, 0)) { ?>
-                 <option value="<?php echo $result ?>" />
+                  while($spdo->query($sql) as $row)) { ?>
+                 <option value="<?php echo $verein ?>" />
                   <?php
                     } 
                   ?>
