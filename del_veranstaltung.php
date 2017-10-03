ca2bddc12eef24de3bbdaf755ca78ee08ade581d
bullet64 <frankmankel@t-online.de>
2017-10-03 12:43:27 +0200

    Update del_veranstaltung.php

--- del_veranstaltung.php
+++ del_veranstaltung.php
@@ -1,29 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-   <meta charset="utf-8">
-   <title>Antwort vom Webserver</title>
-</head>
-<body><p>
    <?php
-      $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-            	
-      echo "Veranstaltung: "           . htmlspecialchars($_POST["veranstaltung"]) . "<br>";
-      $erg = $_POST["veranstaltung"];
-	
-   	$sql = "DELETE FROM veranstaltungen WHERE veranstaltung =  :filmID";
+      	$pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');           	
+      	$sql = "DELETE FROM veranstaltungen WHERE veranstaltung =  :filmID";
 	$stmt = $pdo->prepare($sql);
 	$stmt->bindParam(':filmID', $_POST['veranstaltung'], PDO::PARAM_INT);   
 	$stmt->execute();
-	
-		
 	$pdo = null;
-	
-	
 	?>
 
-</p>
-</body>
-</html>
 
-//$db->query("DELETE FROM veranstaltungen WHERE id='1'");
