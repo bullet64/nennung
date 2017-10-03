520270808f59a424c664861a39e7ea950bcecbaa
bullet64 <frankmankel@t-online.de>
2017-10-03 09:53:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,10 +35,12 @@
   
 
    <?php
+   $order = "verein";
+   $direction = "ASC";
 $pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
-$stmt = $pdo->prepare('SELECT verein FROM vereine SORT BY ?, ?');
-$stmt->bindParam(verein, $offset,PDO::PARAM_INT);
-$stmt->bindParam(ASC, $offset,PDO::PARAM_INT);
+$stmt = $pdo->prepare('SELECT verein FROM vereine SORT BY ".$order ." ".$direction');
+$stmt->bindParam($order, $offset,PDO::PARAM_INT);
+$stmt->bindParam($direction, $offset,PDO::PARAM_INT);
 $stmt->execute();
 ?>
    
