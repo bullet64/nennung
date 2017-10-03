41ad2ba2f7fffb82f548ec6a11154ea570cb96e8
bullet64 <frankmankel@t-online.de>
2017-10-03 12:11:39 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -88,11 +88,32 @@
      
         
       
+   <?php
+$pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
+$stmt = $pdo->query("SELECT veranstaltung FROM veranstaltungen ORDER BY veranstaltung ASC");
+//$stmt->execute();
+?>
    
-    <tr>
+    <form id="idForm" method="post" action="nennung.php">
+    
+   <table>
+     
+      <tr>
            <td>Veranstaltung</td>
-           <td><input name="veranstaltung" size="30" type="text" required> *</td>
-    </tr>
+    <td><input type="text" name="veranstaltung" list="veranstaltungen" required>
+               <datalist id="veranstaltungen">
+                  <?php
+                  while($result = $stmt->fetch(PDO::FETCH_COLUMN, 0)) { ?>
+                 <option value="<?php echo $result ?>" />
+                  <?php
+                    } 
+                 $pdo = null; ?>
+
+               
+               </datalist>
+               *</td>
+      </tr>
+    
       
       <tr>
       <td>&nbsp;</td>
