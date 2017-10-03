3f844282034583430175c2e05a4193f267174e50
bullet64 <frankmankel@t-online.de>
2017-10-03 12:31:03 +0200

    Update del_veranstaltung.php

--- del_veranstaltung.php
+++ del_veranstaltung.php
@@ -12,7 +12,7 @@
 	$erg = $_POST["veranstaltung"];
 		echo $erg;
       
-   $sql="SELECT id FROM veranstaltungen WHERE veranstaltung = @erg];
+   $sql="SELECT id FROM veranstaltungen WHERE veranstaltung = $erg";
    //echo $sql;
 	//$statement = $pdo->prepare("INSERT INTO veranstaltungen (veranstaltung) VALUES (?)");
    //   	$statement->execute(array($_POST["veranstaltung"]));
