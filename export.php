02ec59d43043ebbf002cce49b272c9ed3164cf64
bullet64 <frankmankel@t-online.de>
2017-10-04 17:40:29 +0200

    Update export.php

--- export.php
+++ export.php
@@ -4,6 +4,7 @@
 
 //Definitionen
 $abfrage = array();
+$csv_output = array();
 $filename = 'export.csv';
 $header = array("Section","Lastname","Firstname","Country","EMail","Birthday","Club","Active Frequency","Transponder Nr 1","CarId 1","Transponder Nr 2","CarId 2","Registration","Licence");
 
@@ -48,7 +49,7 @@ foreach ($pdo->query($sql) as $row) {
    
 // Abfrage bauen für den Export. Evt. müssen ein paar Daten angepasst werden.    
     $abfrage[] = $row['veranstaltung'] . "," . $row['vorname'] . "," . $row['nachname'];
-    $csv_output = "\r\n" . implode(",", $abfrage);
+    $csv_output[] = "\r\n" . implode(",", $abfrage[]);
 
 // Daten schreiben
 // Sichergehen, dass die Datei existiert und beschreibbar ist.
