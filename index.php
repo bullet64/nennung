dbdaad511b282d1535ccf5ab95256fa1789c0520
bullet64 <frankmankel@t-online.de>
2017-10-05 17:06:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $stmt = $pdo->query("SELECT veranstaltung FROM veranstaltungen ORDER BY veransta
       
     <tr>
        <td>   
-       <form action="#" id="land" method="post">
+       <form action="#">
           <label>Land
         <td><select name="land">
           <option>DE</option>
