7b9018920394c4b864ac702d2a6f0b060790a42a
bullet64 <frankmankel@t-online.de>
2017-10-05 17:54:00 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,29 +1,70 @@
-<?php
-require_once ('konfiguration.php');
-$db_link = mysqli_connect (
-                     MYSQL_HOST, 
-                     MYSQL_BENUTZER, 
-                     MYSQL_KENNWORT, 
-                     MYSQL_DATENBANK
-                    );
- 
-$sql = "SELECT verein FROM vereine";
- 
-$db_erg = mysqli_query( $db_link, $sql );
-if ( ! $db_erg )
-{
-  die('Ungültige Abfrage: ' . mysqli_error());
-}
- 
-echo '<table border="1">';
-while ($zeile = mysqli_fetch_array( $db_erg, MYSQL_ASSOC))
-{
-  echo "<tr>";
-  echo "<td>". $zeile['id'] . "</td>";
-  echo "<td>". $zeile['verein'] . "</td>";
-  echo "</tr>";
-}
-echo "</table>";
- 
-mysqli_free_result( $db_erg );
+<!DOCTYPE html>
+<html>
+<head>
+   <link rel="stylesheet" href="stylesheet2.css">
+   <meta charset="utf-8">
+   <title>10. NRW-Cup 2018, Anmeldung BETA v0.1</title>
+   
+   
+</head>
+
+<nav>
+   <ul>
+      <li><a href="http://192.168.3.243/nennung/index.php">Home</a></li>
+      <li><a href="http://192.168.3.243/nennung/abfrage.php">Teilnehmerliste</a></li>
+      <li><a href="http://192.168.3.243/nennung/abfrage_h.php">Klasse Hobby</a></li>
+      <li><a href="http://192.168.3.243/nennung/abfrage_t.php">Klasse Top</a></li>
+       <li><a href="http://192.168.3.243/nennung/admin.php">Admin</a></li>
+       <li><a href="http://192.168.3.243/nennung/export.php">RCM Export</a></li>
+          
+              
+  </ul>
+</nav>
+
+<body>
+   <p style="text-align:center">
+   <b>Nennsytem für RC-Car Rennen</b>
+   </p>
+
+   <p style="text-align:center">Die Startgebühr ist <b>VOR</b> dem Wettkampftag zu entrichten.</br>
+    Dazu bitte die Ausschreibung des jeweiligen Vereines beachten!</p>
+
+
+   
+
+  
+
+
+   
+    <form id="idForm" method="post" accept-charset="UTF-8" action="nennung.php">
+    
+   <table>
+     
+      
+   
+   <?php
+$pdo = new PDO('mysql:host=localhost;dbname=nennung', 'bullet64', 'xt19Zkl');
+$stmt = $pdo->query("SELECT veranstaltung FROM veranstaltungen ORDER BY veranstaltung ASC");
+//$stmt->execute();
 ?>
+      
+      <tr>
+       <td>   
+       <form action="nennung.php">
+          <label>Veranstaltung
+        <td><select name="veranstaltung">
+           <option></option>
+            <?php
+                  while($result = $stmt->fetch(PDO::FETCH_COLUMN, 0)) { ?>
+                 <option><?php echo htmlspecialchars($result) ?></option>
+                  <?php
+                    } 
+                 $pdo = null; ?>
+                
+             </select>
+      </label>
+    </form>
+       </td>
+    </tr> 
+     
+     </body>
