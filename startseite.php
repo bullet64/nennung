0350f511031fd699ad6575105e1cd07ff519c698
bullet64 <frankmankel@t-online.de>
2017-10-06 22:00:41 +0200

    Update startseite.php

--- startseite.php
+++ startseite.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-   <link rel="stylesheet" href="stylesheet2.css">
+   
    <meta charset="utf-8">
    <title>Startseite Nennsystem BETA v0.1</title>
  
