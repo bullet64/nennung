e35a12a762b06e34c6689bb96902f0d5a03fcf79
bullet64 <frankmankel@t-online.de>
2017-10-06 15:46:32 +0200

    Update abfrage_h.php

--- abfrage_h.php
+++ abfrage_h.php
@@ -30,17 +30,21 @@ $sql = "SELECT * FROM nennungen WHERE klasse = 'OR8 Hobby' ";
   <tbody>
 	  <tr>
 		  <th>ID</th>
-		  <th>Name</th>
-		  <th>Vorname</th>
-		  <th>Geburtstag</th>
-		  <th>DMC</th>
-		  <th>Verein</th>
-		  <th>Transponder_ID</th>
-		  <th>Klasse</th>
-		  <th>EMail</th>
-		  <th>Car_ID</th>
-		  <th>Nenndatum</th>
-		  <th>Veranstaltung</th>
+                  <th>Nachname</th>
+                  <th>Vorname</th>
+                  <th>Geburtstag</th>
+                  <th>DMC</th>
+                  <th>Verein</th>
+                  <th>Transponder_ID1</th>
+                  <th>Car_ID1</th>
+                  <th>Transponder ID2</th>
+                  <th>Car_ID2</th>
+                  <th>Klasse</th>
+                  <th>EMail</th>
+                  <th>Veranstaltung</th>
+                  <th>Land</th>           
+                  <th>Nenndatum</th>
+
 		  		  	
 	  </tr>
 	  
@@ -60,12 +64,18 @@ $sql = "SELECT * FROM nennungen WHERE klasse = 'OR8 Hobby' ";
        
 	<td><?=$erg?></td>
 	
-	<td><?=$row['dmc']?></td>
-	<td><?=$row['verein']?></td>
-	<td><?=$row['transponder_id']?></td>
-	<td><?=$row['klasse']?></td>
-	<td><?=$row['email']?></td>
-	<td><?=$row['car_id']?></td>
+	    <td><?=$row['dmc']?></td>
+        <td><?=$row['verein']?></td>
+        <td><?=$row['transponder_id1']?></td>
+        <td><?=$row['car_id1']?></td>
+        <td><?=$row['transponder_id2']?></td>
+        <td><?=$row['car_id2']?></td>
+        <td><?=$row['klasse']?></td>
+        <td><?=$row['email']?></td>
+        <td><?=$row['veranstaltung']?></td>
+        <td><?=$row['land']?></td>
+
+	
 	    
 	<?php
 	//Meldedatum zerlegen! Input = 2017-09-30 21:18:46.735059
@@ -79,7 +89,7 @@ $sql = "SELECT * FROM nennungen WHERE klasse = 'OR8 Hobby' ";
 		
 	    ?>   
 	<td><?=$ergx?></td>
-	<td><?=$row['veranstaltung']?></td> 
+	
 	    
 	  </tr>
     <?php endforeach;
