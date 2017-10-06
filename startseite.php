07d184b71ef1c3ca81594501b85af972d4e604e9
bullet64 <frankmankel@t-online.de>
2017-10-06 19:34:40 +0200

    Update startseite.php

--- startseite.php
+++ startseite.php
@@ -66,9 +66,7 @@
 	    <td><?=$row['teilnehmer_max']?></td>
        <td><?=$row['teilnehmer']?></td>
     
-	  <p><a href="https://www.w3schools.com/html/">Visit our HTML tutorial</a></p>
-	  </tr>
-	  
+	  	  
     <?php endforeach;?>
 	  
 	  
