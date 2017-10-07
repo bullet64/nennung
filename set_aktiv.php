4ee789cec14cfbdc4b09401c1a436c3fdc0db571
bullet64 <frankmankel@t-online.de>
2017-10-07 09:59:41 +0200

    Create set_aktiv.php

---
+++ set_aktiv.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Antwort vom Webserver</title>
+</head>
+<body>
+	<?php include ("connect.php");?>
+	<p>
+   <?php
+      
+            	
+      echo "Aktiv: "           . htmlspecialchars($_POST["aktiv"]) . "<br>";
+      
+      
+   
+	
+	
+	
+	?>
+
+</p>
+</body>
+</html>
